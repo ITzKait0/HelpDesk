21a95b6a0a3614fabfc99452d55f6a2331bccb87
ITzKait0 <ben180701@gmail.com>
2026-03-09 09:42:04 +0100

    worked on prasentation

--- frontend/src/pages/CreateTicket.tsx
+++ frontend/src/pages/CreateTicket.tsx
@@ -3,7 +3,6 @@ import { addTicket } from '../API';
 import logo from '../assets/logo.png'
 import './CreateTicket.css'
 
-// import { useState } from 'react';
 
 function CreateTicket() {
     const [status, setStatus] = useState("idle");
@@ -41,30 +40,27 @@ function CreateTicket() {
               <input id="topic" placeholder='Betreff'/>
               <textarea id="textfield" className="textfield" rows={10} placeholder='Text'/>
           </div>
-          <div className='card'>
-            {status === "pending" && (
-              <Typography variant="body1" color="textSecondary" align="center" sx={{ mt: 2 }}>
-                Ticket wird erstellt...
-              </Typography>
-            )}
-            {status === "success" && (
-              <div>
-              <Typography variant="body1" color="success.main" align="center" sx={{ mt: 2 }}>
-                Ticket erfolgreich erstellt! 
-              </Typography>
-              <button  onClick={()=>setStatus("idle")}>ok</button>
-              </div>
-            )}
-            {status === "idle" && (
-              <button  id='createTicket' onClick={async() => createTicket()}>
-                neues Ticket erstellen
-              </button> 
-            )}
-          </div>
-        </Paper>
-     </Box>
-    </Grid>
-      
+          <div className="card">
+              {status ==="pending" &&(
+                <div>Ticket wird erstellt...</div>
+              )}
+              {status ==="success" && (
+                <div>
+                  <div>Ticket erstellt!</div>
+                  <button onClick={()=>{
+                    setStatus("idle")}}>
+                      ok
+                  </button>
+                </div>
+              )}
+              {status ==="idle" && (
+                <button id ="createTicket"onClick={async() => createTicket()}>
+                  neues Ticket erstellen         
+                </button>
+              )}
+              
+      </div>
+    </>
   )
 }
 
